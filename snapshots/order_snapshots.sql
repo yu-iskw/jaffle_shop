9e569b21b421d2ebfdc6d9c2f67e1f925b9e225b
Claire Carroll <carroll.claire.e@gmail.com>
2019-07-15 10:18:21 -0400

    wip

---
+++ snapshots/order_snapshots.sql
@@ -0,0 +1,14 @@
+{% snapshot orders_snapshot %}
+
+{{
+    config(
+      target_schema='snapshots',
+      unique_key='id',
+      strategy='timestamp',
+      updated_at='_elt_updated_at',
+    )
+}}
+
+select * from {{ source('raw_jaffle_shop', 'orders') }}
+
+{% endsnapshot %}
